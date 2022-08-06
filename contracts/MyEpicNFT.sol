465a7d28a8653b384548a9414022043ab73d9294
raghav <raghavjindal0212@gmail.com>
2022-08-06 15:52:11 +0530

    .

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -7,8 +7,6 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-import "hardhat/console.sol";
-
 import {Base64} from "./libraries/Base64.sol";
 
 contract MyEpicNFT is ERC721URIStorage {
